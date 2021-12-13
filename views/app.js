13c2fd69605926e708576fb4068ae167124bafea
Rudzix66 <kubadabrowa14@gmail.com>
2021-12-13 22:17:37 +0100

    fix

--- views/app.js
+++ views/app.js
@@ -5,19 +5,31 @@ let userName = "";
 let userRank = "";
 
 function checkUserData() {
-  while(userName === null || userRank === null || userName === "" || userRank === "")
   if (userName == "" || userRank == "") {
     userName = prompt("Podaj swoje imię");
     userRank = prompt("Podaj swoją rangę");
-    if (userName === null || userRank === null) {
+  }
+  if (userName === null || userRank === null) {
       alert("Nie podano danych!");
-    }
 
+    policeManRank.innerHTML = `Stopień: ${userRank}`;
+    policeManName.innerHTML = `Funkcjnariusz: ${userName}`;
+  } else{
     policeManRank.innerHTML = `Stopień: ${userRank}`;
     policeManName.innerHTML = `Funkcjnariusz: ${userName}`;
   }
 }
 checkUserData();
 
-const lookedPearson = document.getElementById("SearchPearson").value;
-const pearson = lookedPearson.toString();
+function debounce(func, timeout = 300){
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout)
+  };
+}
+const processChange = debounce(() => {
+  const lookedPearson = document.getElementById("SearchPearson").value;
+  const pearson = lookedPearson.toString();
+  console.log(lookedPearson);
+});
