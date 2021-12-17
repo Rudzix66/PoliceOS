45bb9fe0d3309121aedc2476d3f582e0023538d1
Wojciech <76787469+Wtyka2504@users.noreply.github.com>
2021-12-17 13:40:13 +0100

    commit

--- workspace/scripts/app.js
+++ workspace/scripts/app.js
@@ -1,94 +1,107 @@
-(function () {
-  const alertBox = document.querySelector(".alert-box");
-  const alertInputs = alertBox.querySelectorAll("input");
-  const alertBtn = alertBox.querySelector("button");
-  const userName = document.querySelector("#user-name");
-  const userStatus = document.querySelector("#user-status");
-  const localUser = localStorage.getItem("user");
-  const editUserInfo = document.querySelector(".edit-user-info");
+( function ()
+{
+  const alertBox = document.querySelector( ".alert-box" );
+  const alertInputs = alertBox.querySelectorAll( "input" );
+  const alertBtn = alertBox.querySelector( "button" );
+  const userName = document.querySelector( "#user-name" );
+  const userStatus = document.querySelector( "#user-status" );
+  const localUser = localStorage.getItem( "user" );
+  const editUserInfo = document.querySelector( ".edit-user-info" );
 
-  if (!localUser) {
-    alertInputs.forEach((input) => {
-      input.addEventListener("keyup", debounce(alertInputEvent));
-      input.addEventListener("change", debounce(alertInputEvent));
-    });
+  if ( !localUser )
+  {
+    alertInputs.forEach( ( input ) =>
+    {
+      input.addEventListener( "keyup", debounce( alertInputEvent ) );
+      input.addEventListener( "change", debounce( alertInputEvent ) );
+    } );
 
-    alertBtn.addEventListener("click", function () {
-      const name = [...alertInputs].filter(
-        (input) => input.dataset.name === "name"
+    alertBtn.addEventListener( "click", function ()
+    {
+      const name = [ ...alertInputs ].filter(
+        ( input ) => input.dataset.name === "name"
       );
-      const status = [...alertInputs].filter(
-        (input) => input.dataset.name === "status"
+      const status = [ ...alertInputs ].filter(
+        ( input ) => input.dataset.name === "status"
       );
-      const nameVal = name[0].value.trim();
-      const statusVal = status[0].value.trim();
+      const nameVal = name[ 0 ].value.trim();
+      const statusVal = status[ 0 ].value.trim();
 
       userName.textContent = nameVal;
       userStatus.textContent = statusVal;
 
       localStorage.setItem(
         "user",
-        JSON.stringify({ name: nameVal, status: statusVal })
+        JSON.stringify( { name: nameVal, status: statusVal } )
       );
 
       alertBox.remove();
-    });
-  } else {
-    const userData = JSON.parse(localUser);
+    } );
+  } else
+  {
+    const userData = JSON.parse( localUser );
     userName.textContent = userData.name;
     userStatus.textContent = userData.status;
     alertBox.remove();
   }
 
-  function alertInputEvent() {
-    const isEmpty = [...alertInputs].every((el) => el.value);
-    if (!isEmpty) {
-      alertBtn.classList.add("disabled");
-    } else {
-      alertBtn.classList.remove("disabled");
+  function alertInputEvent ()
+  {
+    const isEmpty = [ ...alertInputs ].every( ( el ) => el.value );
+    if ( !isEmpty )
+    {
+      alertBtn.classList.add( "disabled" );
+    } else
+    {
+      alertBtn.classList.remove( "disabled" );
     }
   }
 
-  editUserInfo.addEventListener("click", () => {
-    const html ='<div class="alert-box"><div class="box"><div class="box-bar">Wypełnij dane</div><div class="box-content"><label><span>Wpisz imię i nazwisko:</span><input data-name="name" type="text" autocomplete="name"></label><label><span>Wpisz stopień:</span><input data-name="status" type="text" autocomplete="status"></label><button type="submit">Zapisz</button></div></div></div>'
+  editUserInfo.addEventListener( "click", () =>
+  {
+    const html = '<div class="alert-box"><div class="box"><div class="box-bar">Wypełnij dane</div><div class="box-content"><label><span>Wpisz imię i nazwisko:</span><input data-name="name" type="text" autocomplete="name"></label><label><span>Wpisz stopień:</span><input data-name="status" type="text" autocomplete="status"></label><button type="submit">Zapisz</button></div></div></div>'
 
 
-  document.body.insertAdjacentHTML("beforeend",html);
+    document.body.insertAdjacentHTML( "beforeend", html );
 
-  alertInputs.forEach((input) => {
-    input.addEventListener("keyup", debounce(alertInputEvent));
-    input.addEventListener("change", debounce(alertInputEvent));
-  });
+    alertInputs.forEach( ( input ) =>
+    {
+      input.addEventListener( "keyup", debounce( alertInputEvent ) );
+      input.addEventListener( "change", debounce( alertInputEvent ) );
+    } );
 
-  alertBtn.addEventListener("click", function () {
-    const name = [...alertInputs].filter(
-      (input) => input.dataset.name === "name"
-    );
-    const status = [...alertInputs].filter(
-      (input) => input.dataset.name === "status"
-    );
-    const nameVal = name[0].value.trim();
-    const statusVal = status[0].value.trim();
+    alertBtn.addEventListener( "click", function ()
+    {
+      const name = [ ...alertInputs ].filter(
+        ( input ) => input.dataset.name === "name"
+      );
+      const status = [ ...alertInputs ].filter(
+        ( input ) => input.dataset.name === "status"
+      );
+      const nameVal = name[ 0 ].value.trim();
+      const statusVal = status[ 0 ].value.trim();
 
-    userName.textContent = nameVal;
-    userStatus.textContent = statusVal;
+      userName.textContent = nameVal;
+      userStatus.textContent = statusVal;
 
-    localStorage.setItem(
-      "user",
-      JSON.stringify({ name: nameVal, status: statusVal })
-    );
+      localStorage.setItem(
+        "user",
+        JSON.stringify( { name: nameVal, status: statusVal } )
+      );
 
-    alertBox.remove();
-  });
-  })
+      alertBox.remove();
+    } );
+  } )
 
 
-})();
+} )();
 
-function debounce(fn = () => {}, delay = 300) {
+function debounce ( fn = () => { }, delay = 300 )
+{
   let timer;
-  return (...args) => {
-    window.clearTimeout(timer);
-    setTimeout(fn.call(...args), delay);
+  return ( ...args ) =>
+  {
+    window.clearTimeout( timer );
+    setTimeout( fn.call( ...args ), delay );
   };
 }
--- workspace/scripts/components/alterbox.js
+++ workspace/scripts/components/alterbox.js
@@ -1,22 +1,45 @@
-function AlterBox ( o = {} )
+class AlterBox extends HTMLElement
 {
-  const html = `
-  <div class="alert-box">
-      <div class="box">
-          <div class="box-bar">
-              ${ o.title }
-          </div>
-          <div class="box-content">
-              <label>
-                  <span>Wpisz imię i nazwisko:</span>
-                  <input data-name="name" type="text" autocomplete="name">
-              </label>
-              <label>
-                  <span>Wpisz stopień:</span>
-                  <input data-name="status" type="text" autocomplete="status">
-              </label>
-              <button type="submit">Zapisz</button>
-          </div>
-      </div>
-  </div>`;
+  constructor ( data = {} )
+  {
+    super();
+    this.innerHTML = `    
+    <div class="alert-box">
+            <div class="box">
+                <div class="box-bar">
+                    ${ replaceEmpty( data.title, "Jakiś tytuł jeśli nie będzie" ) }
+                </div>
+                <div class="box-content">
+                    <label>
+                        <span>Wpisz imię i nazwisko:</span>
+                        <input data-name="name" type="text" autocomplete="name">
+                    </label>
+                    <label>
+                        <span>Wpisz stopień:</span>
+                        <input data-name="status" type="text" autocomplete="status">
+                    </label>
+                    <button type="submit">${ replaceEmpty( data.submit, "Nazwa przycisku" ) }</button>
+                </div>
+            </div>
+        </div>`;
+  }
+
+  exampleEvent ()
+  {
+    this.querySelector( "button" ).addEventListener( "click", () =>
+    {
+      console.log( "button kliknięty" );
+    } )
+  }
 }
+
+function replaceEmpty ( value, replace )
+{
+  if ( !value )
+  {
+    return replace;
+  }
+  return value;
+}
+
+customElements.define( "alter-box", AlterBox );
