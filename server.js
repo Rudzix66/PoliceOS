82a627e748c2195addf08ab09969634ba0be6b15
Wojciech <76787469+Wtyka2504@users.noreply.github.com>
2021-12-22 22:59:19 +0100

    eklfsj

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ app.get( "/users/:id", ( req, res ) =>
           db.all( "SELECT * FROM fines WHERE userId = ?;", [ parseInt( row.id ) ], function ( err, result )
           {
             if ( err )
-              row.arrest = 0;
+              row.fines = 0;
             else
-              row.arrest = result.length;
+              row.fines = result.length;
           } );
           db.all( "SELECT * FROM arrest WHERE userId = ?;", [ parseInt( row.id ) ], function ( err, result )
           {
