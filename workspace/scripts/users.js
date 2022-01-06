9654366324fdb6450f883d7ff42da9f8ddffe47e
Rudzix66 <kubadabrowa14@gmail.com>
2022-01-06 20:27:43 +0100

    commit

---
+++ workspace/scripts/users.js
@@ -0,0 +1,17 @@
+post("/users",{ 
+    action: "add",
+    first_name: "", 
+    last_name: "",
+    birth_date: ""
+})
+
+post("/users",{ 
+    action: "update",
+    name: "Dawid Dąbrowa",
+    value: ""
+})
+
+post("/users",{ 
+    action: "delete",
+    id: 20
+})
